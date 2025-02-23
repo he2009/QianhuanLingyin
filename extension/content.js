a64090ea132360dff4b0e6584c56d1b82fd130e0
diandian157 <X94106038@gmail.com>
2025-02-23 11:04:34 +0800

    简写判断，让其他样式默认读取十周年参数

--- extension/content.js
+++ extension/content.js
@@ -469,7 +469,7 @@ export let CONTENT = function(config, pack) {
 							}
 							// @ts-ignore
 							var editSkin = game.qhly_getSkin(game.qhly_getRealName(avatars[i]));
-							var theme = ui.arena.dataset.newDecadeStyle == 'on' ? 'decade' : 'decade';
+							var theme = 'decade';
 							// @ts-ignore
 							if (lib.config['extension_千幻聆音_qhly_editDynamic'] && lib.qhly_skinEdit[game
 									.qhly_getRealName(avatars[i])] && lib.qhly_skinEdit[game
@@ -1467,7 +1467,7 @@ export let CONTENT = function(config, pack) {
 								.selectSkinData.value = keys[i];
 						}
 					}
-					var theme = ui.arena.dataset.newDecadeStyle == 'on' ? 'decade' : 'decade';
+					var theme = 'decade';
 					// @ts-ignore
 					if (lib.config['extension_千幻聆音_qhly_editDynamic'] && lib.qhly_skinEdit[realName] && lib
 						.qhly_skinEdit[realName][skinName] && lib.qhly_skinEdit[realName][skinName].player && lib
@@ -1940,7 +1940,7 @@ export let CONTENT = function(config, pack) {
 				if (skinCopy.action && skinCopy.pos) skinCopy.action = "ChuChang";
 				if (skinCopy.speed == undefined) skinCopy.speed = 1;
 				skinCopy.player = skinCopy;
-				var theme = ui.arena.dataset.newDecadeStyle == 'on' ? 'decade' : 'shousha';
+				var theme = 'decade';
 				// @ts-ignore
 				if (lib.config['extension_千幻聆音_qhly_editDynamic'] && lib.qhly_skinEdit[realName] && lib
 					.qhly_skinEdit[realName][skinName] && lib.qhly_skinEdit[realName][skinName].player && lib
